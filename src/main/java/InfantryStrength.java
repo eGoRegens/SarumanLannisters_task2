81a217259cd37ba3017542d0042316c5ce2d25c7
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-30 19:44:40 +0400

    gitignor add and task

---
+++ src/main/java/InfantryStrength.java
@@ -0,0 +1,12 @@
+public class InfantryStrength {
+    public static void main(String[] args) {
+        int infantryLannisters = 1207;
+        int orksSaruman = 2000;
+        int totalNumberOfInfantry = infantryLannisters + orksSaruman;
+
+        System.out.println("Численность пехоты у Ланнистеров составляет - " + infantryLannisters + " единиц.");
+        System.out.println("Численность пехоты Сарумана равна " + orksSaruman + " единицам.");
+        System.out.println("Общая численность пехоты Ланнистеров и Сарумана равна " + totalNumberOfInfantry + " единицам.");
+
+    }
+}

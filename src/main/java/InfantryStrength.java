efaec7008c934cf87057963d3a1e2b69b7a9d3dd
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-30 23:05:26 +0400

    назначение констант

--- src/main/java/InfantryStrength.java
+++ src/main/java/InfantryStrength.java
@@ -1,11 +1,11 @@
 public class InfantryStrength {
     public static void main(String[] args) {
-        int infantryLannisters = 1207;
-        int orksSaruman = 2000;
-        int totalNumberOfInfantry = infantryLannisters + orksSaruman;
+       final int INFANTRY_LANNISTERS = 1207;
+       final int ORKS_SARUMAN = 2000;
+       final int TOTAL_INFANTRY = INFANTRY_LANNISTERS + ORKS_SARUMAN;
 
-        System.out.println("Численность пехоты у Ланнистеров составляет - " + infantryLannisters + " единиц.");
-        System.out.println("Численность пехоты Сарумана равна " + orksSaruman + " единицам.");
-        System.out.println("Общая численность пехоты Ланнистеров и Сарумана равна " + totalNumberOfInfantry + " единицам.");
+        System.out.println("Численность пехоты у Ланнистеров составляет - " + INFANTRY_LANNISTERS + " единиц.");
+        System.out.println("Численность пехоты Сарумана равна " + ORKS_SARUMAN + " единицам.");
+        System.out.println("Общая численность пехоты Ланнистеров и Сарумана равна " + TOTAL_INFANTRY + " единицам.");
     }
 }

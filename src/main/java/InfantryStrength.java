f12ac5acbdc51b5b8f798b491302496ff0aff885
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-10-31 11:15:42 +0400

    оптимизация кода

--- src/main/java/InfantryStrength.java
+++ src/main/java/InfantryStrength.java
@@ -1,8 +1,8 @@
 public class InfantryStrength {
     public static void main(String[] args) {
-       final int INFANTRY_LANNISTERS = 1207;
-       final int ORKS_SARUMAN = 2000;
-       final int TOTAL_INFANTRY = INFANTRY_LANNISTERS + ORKS_SARUMAN;
+        final int INFANTRY_LANNISTERS = 1207;
+        final int ORKS_SARUMAN = 2000;
+        final int TOTAL_INFANTRY = INFANTRY_LANNISTERS + ORKS_SARUMAN;
 
         System.out.println("Численность пехоты у Ланнистеров составляет - " + INFANTRY_LANNISTERS + " единиц.");
         System.out.println("Численность пехоты Сарумана равна " + ORKS_SARUMAN + " единицам.");
